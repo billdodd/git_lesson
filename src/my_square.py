b44845d4a64d6ba7ff2ed30a334adbb5111c545a
Bill Dodd <billdodd@gmail.com>
2017-03-25 16:23:45 -0500

    change variable x to z and square 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,9 +1,9 @@
 
-def my_square(x):
+def my_square(z):
     """
     takes an integer value and returns the sqared value
     uses the ** operator
     """
-    return(x ** 2)
+    return(z * z)
 
-print(my_square(4))
+print(my_square(24))
